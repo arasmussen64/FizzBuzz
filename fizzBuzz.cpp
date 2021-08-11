06343ec45fd4819ef560c6120e1a07bdf7b5f973
arasmussen64 <arasmussen64@gmail.com>
2021-08-11 11:12:42 -0600

    initial

---
+++ fizzBuzz.cpp
@@ -0,0 +1,11 @@
+/*Count 1-100 and every number divisible by 3 is replaced with the word fizz.
+Every number divisible by 5 is replaced with buzz.
+And every number divisible by 15 is replaced with fizzbuzz.*/
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 100; i++) {
+
+    }
+}
